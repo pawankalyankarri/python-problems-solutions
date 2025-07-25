4e87975439b9c8237cdd48d5f5808c1e9660ca8e
pawankalyan <pawankalyankarri22@gmail.com>
2025-07-26 00:17:22 +0530

    added few pattern problems

--- patterns.py
+++ patterns.py
@@ -20,13 +20,93 @@ rows = int(input('Enter a number: '))
 #         print(' '*(i-middle)+'*'*(2*rows-2*i+1))  #22 - 12+1 =>11
 
 
-middle = (rows//2)+1
-for i in range(1,rows+1):
-    if i == 1 or i == rows:
-        print(' '*(middle-1)+'*',sep='')
-    elif i<middle:
-        print(' '*(middle-i)+'*'+' '*(2*i-3)+'*',sep='')
-    else:
-        print(' '*(i-middle)+'*'+' '*(2*rows-2*i-1)+'*',sep='')
+# middle = (rows//2)+1
+# for i in range(1,rows+1):
+#     if i == 1 or i == rows:
+#         print(' '*(middle-1)+'*',sep='')
+#     elif i<middle:
+#         print(' '*(middle-i)+'*'+' '*(2*i-3)+'*',sep='')
+#     else:
+#         print(' '*(i-middle)+'*'+' '*(2*rows-2*i-1)+'*',sep='')
         
     
+    
+    
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+
+
+# c = 1
+
+# for i in range(1,rows+1):
+#     for j in range(i):
+#         print(c,end='')
+#         c+=1
+#     print()
+    
+
+# for i in range(1,rows+1):
+#     for j in range(1,i+1):
+#         print(j,end='')
+#     print()
+
+
+
+# c = 1
+# for i in range(1,rows+1):
+#     for j in range(i):
+#         print(c,end='')
+#         c+=1
+#     print()
+    
+    
+    
+    
+# for i in range(rows,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end='')
+#     print()
+    
+    
+    
+# for i in range(1,rows+1):
+#     print('*'*i)
+
+    
+    
+# for i in range(1,rows+1):
+#     print(' '*(rows-i)+'*'*(2*i-1))
+
+
+# for i in range(1,rows+1):
+#     print(' '*(i-1)+'*'*(2*rows-2*i+1))
+
+
+# for i in range(1,rows+1):
+#     for j in range(1,i+1):
+#         print(chr(64+j),end='')
+#     print()
+
+
+# middle = (rows//2)+1
+# for i in range(1,rows+1):
+#     if i<middle:
+#         print(' '*(middle-i)+'*'*(2*i-1))
+#     else:
+#         print(' '*(i-middle)+'*'*(2*rows-2*i+1))
+    
+
+# for i in range(1,rows+1):
+#     for j in range(1,i+1):
+#         print(chr(64+j),end='')
+#     print()
+
+
+# for i in range(1,rows+1):
+#     print(' '*(i-1)+'*'*(2*rows-2*i+1))
+
+
+
+
